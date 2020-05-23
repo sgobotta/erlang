438aac112b55d9b346c83079fd1222dd3a1d8e7d
sgobotta <santiago@camba.coop>
2020-05-23 16:43:12 -0300

    :computer: removes error message

--- steps/3/3.10/txt.erl
+++ steps/3/3.10/txt.erl
@@ -20,15 +20,15 @@ format_fills_lines_with_a_given_length_test() ->
   test_format(20),
   test_format(10),
   test_format(69),
-  ?assertThrow({error, "lala"}, test_format(9)),
-  ?assertThrow({error, _}, test_format(5)),
-  ?assertThrow({error, _}, test_format(0)).
+  ?assertThrow({error}, test_format(9)),
+  ?assertThrow({error}, test_format(5)),
+  ?assertThrow({error}, test_format(0)).
 
 assertLinesLength([], _) -> void;
 assertLinesLength([Line|Lines], MaxLineLength) when length(Line) =< MaxLineLength ->
   ?assert(length(Line) =< MaxLineLength),
   assertLinesLength(Lines, MaxLineLength);
-assertLinesLength(_,_) -> throw({error, "lala"}).
+assertLinesLength(_,_) -> throw({error}).
 
 
 get_file_string(FileName) ->
